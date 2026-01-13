a82640b19c5aed83d08edb1862527bc56d021edd
Stan Egorov <segorov@paloaltonetworks.com>
2026-01-13 10:05:12 -0800

    Migrate from Jekyll to Astro + Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                earth: '#7c6f64',
+                sand: '#d6c6a8'
+            }
+        }
+    },
+    plugins: []
+};
