b0c449336fce5ae0d929f69501c8bdac764fdc40
Stan Egorov <realsystem@gmail.com>
2026-01-13 10:51:54 -0800

    debug timeout for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    site: 'https://realsystem.github.io',
-    output: 'static',
-    outDir: 'dist'
+  site: 'https://realsystem.github.io',
+  output: 'static',
+  outDir: 'dist',
+  integrations: [tailwind()]
 });
